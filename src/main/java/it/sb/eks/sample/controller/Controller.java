1ed1dc143b4d289734a6217264e90f39ba90be48
stefanobaiardo <baiardo.stefano@gmail.com>
2022-07-12 09:58:40 +0200

    WIP

--- src/main/java/it/sb/eks/sample/controller/Controller.java
+++ src/main/java/it/sb/eks/sample/controller/Controller.java
@@ -5,16 +5,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/v1")
-@Slf4j
+//@RequestMapping("/v1")
+//@Slf4j
 public class Controller {
 
     @GetMapping("/test")
     public ResponseEntity<?> test(){
 //        log.info("TEST =======================> OK");
         return new ResponseEntity<>("Test OK", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/test2", method = RequestMethod.GET)
+    public ResponseEntity<?> getResource(){
+        return new ResponseEntity<>("Test 2 OK", HttpStatus.OK);
     }
 }
